e7ed19df3d9f7a9fa1267d9b459cdb7f239572f1
Roman Kamyk <roman.kamyk@gmail.com>
2011-10-16 23:13:04 +0200

    opencv examples

---
+++ examples/dispim.cpp
@@ -0,0 +1,9 @@
+#include "highgui.h"
+int main( int argc, char** argv ) {
+  IplImage* img = cvLoadImage( argv[1] );
+  cvNamedWindow( "Example1", CV_WINDOW_AUTOSIZE );
+  cvShowImage( "Example1", img );
+  cvWaitKey(0);
+  cvReleaseImage( &img );
+  cvDestroyWindow( "Example1" );
+}
